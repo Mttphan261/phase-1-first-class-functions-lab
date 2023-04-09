b0070802ec83690b9e97659728144175f903195f
Matt Phan <mttphan261@gmail.com>
2023-04-09 11:27:54 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function([Antonia, Nuru, Amari, Mo]) {
+    return [Antonia, Nuru ];
+}
+const returnLastTwoDrivers = function([Antonia, Nuru, Amari, Mo]) {
+    return [Amari, Mo ];
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(fareMultiplier) {
+    return function(fare) {
+        return fare * fareMultiplier;
+    }
+}
+
+const initialFare = createFareMultiplier();
+
+function fareDoubler(initialFare) {
+    return initialFare * 2;
+}
+
+function fareTripler(initialFare) {
+    return initialFare * 3;
+}
+
+function selectDifferentDrivers([Antonia, Nuru, Amari, Mo], driverFunction) {
+    if (driverFunction === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers ([Antonia, Nuru, Amari, Mo])
+    }
+    else if (driverFunction === returnLastTwoDrivers) {
+        return returnLastTwoDrivers ([Antonia, Nuru, Amari, Mo])
+
+    }
+}
+
